ae48a2d7b5b129bc6a65128a1425ae34dec5bf8f
MD Waliullah <waliullah.godev@gmail.com>
2025-12-16 09:58:42 +0600

    more with time complexity

--- module-2/vector.cpp
+++ module-2/vector.cpp
@@ -4,14 +4,17 @@ using namespace std;
 int main()
 {
 
-    vector<int> v;
+    vector<int> v; // O(1)
 
-    vector<int> v1(10);
+    vector<int> v1(10); // O(N)
 
-    vector<int> v2(10, 7);
+    vector<int> v2(10, 7); // O(N)
 
-    
+    int a[5] = {1, 3, 5, 2, 8};
 
+    vector<int> v3(a, a + 5); // O(N)
+
+    vector<int> v4 = {1, 3, 5, 2, 8};  // O(N)
 
     return 0;
 }
